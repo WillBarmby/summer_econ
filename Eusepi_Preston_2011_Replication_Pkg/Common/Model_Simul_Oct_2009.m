6566905fff7b9aeb77bd602538cedc96461cd452
Will Barmby <willbarmby@gmail.com>
2026-07-01 13:45:38 -0400

    Remove MATLAB global state

--- Eusepi_Preston_2011_Replication_Pkg/Common/Model_Simul_Oct_2009.m
+++ Eusepi_Preston_2011_Replication_Pkg/Common/Model_Simul_Oct_2009.m
@@ -1,84 +1,77 @@
- 
+
 
 
  %%% THIS FUNCTION SIMULATES A MODEL (defined by Model function) GIVEN THE
- %%% SET OF PARAMETERS DEFINED IN x 
+ %%% SET OF PARAMETERS DEFINED IN x
 
- 
-  
-  
+
+
+
 
 
      function [Y_var,Exp_R_1Q,Exp_R_3Q,Exp_w_1Q,Exp_w_2Q,Exp_w_3Q,...
          Exp_w_4Q,Exp_rk_1Q,Exp_rk_2Q,Exp_rk_3Q,Exp_rk_4Q,...
               Regressors_ini,R_mat_ini,state_ini,OMEGA_c_ini,OMEGA_0_ini,ct2] = ...
-         Model_Oct_Sept_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L);
-
-    global epsZ
-
-
-  
-    
-    
+         Model_Oct_Sept_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L,epsZ);
 %% COMMENT BELOW IF USING AS A FUNCTION!
 
 %    clear all
 %   %clc
-%   
+%
 % %% Simulation parameters (general)
 %  exp_gen = 1;
-%  
-%  imp_resp = 0; 
-%  
+%
+%  imp_resp = 0;
+%
 %  full = 1;
-%  
-%  
-%  
-%  
+%
+%
+%
+%
 %  %% the parameters below are not needed (they are assigned arbitrary values)
 %  epsZ_imp1 = 1;
-%  
-%  ini1 = 0; ini2 = 0; ini3 = 0; ini4 = 0; 
-%  
+%
+%  ini1 = 0; ini2 = 0; ini3 = 0; ini4 = 0;
+%
 %  sim_L = 1;
-% 
-% 
+%
+%
 % opt_x = exp(-0.034);
-% 
+%
 % %  param(1) = 1;  %% IH
 % %  param(2) = 0; %% External effects
 % %  param(3) = 1.2; %% sigma
 % %  param(4) = 0; %% simple RBC (Takes 1 or zero)
 % % param(5) = 0.01; %% frish labor supply elasticity
 % %  param(6) = 0; %% 0 for standard nonsep, 1 for partic. model
-%  
+%
 %   x = [1;0;1;1;0.01;0.0049]; %% parameters full model
-%   
-%  S_mat = [(opt_x)]';  
-%  
+%
+%  S_mat = [(opt_x)]';
+%
 %  fb = 1;
-%  
+%
 %  lern = 1;
-%  
-%  
-% 
-%   
+%
+%
+%
+%
 % epsZ = randn(1,22000);
 %  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
- 
+
 
  gs = 0;
- 
- 
- 
+
+
+
 % Control on bounds for calibrated coefficients
- ct2 = 0; 
-  
- 
- 
+ ct2 = 0;
+
+
+
 %%%%%%%%%%%%%%%%%%%%%--- OPTIONS ---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -87,24 +80,24 @@ gen_shocks = gs; %% set to 1 to generate new sequence of shocks
 learning = lern;  %% set == 0 for simulation under REE (no learning update)
 
 feedback = fb;  %% set == 0 for NO feedbacks (active if learning ==1)
-               %% NOTE: this gives the option of simulating the model under 
+               %% NOTE: this gives the option of simulating the model under
                %% REE while estimating the PLM recursively.
 
 cg = 1;        %% set == 1 if constant gain (active if learning ==1)
 
 sga = 0;       %% set == 1 is stochastig gradient (active if learning ==1)
 
-n_c = 1;       %% set == 1 if want to include the constant in the agent's 
+n_c = 1;       %% set == 1 if want to include the constant in the agent's
                %% regression.
 
 p_f = 0;       %% set == 1 to activate projection facility
-               %% NOTE: also computes the beliefs' distribution in the case 
+               %% NOTE: also computes the beliefs' distribution in the case
                %% of no-feedbacks from learning
-               %% (active if learning ==1)   
-
-pf_k = 8;   %% defines how tight is the projection facility               
                %% (active if learning ==1)
-               
+
+pf_k = 8;   %% defines how tight is the projection facility
+               %% (active if learning ==1)
+
 store = 0;   %% set == 1 to store the coefficients (active if learning==1)
 
 
@@ -118,9 +111,9 @@ R_mat_tol = 1.0856345e-010; %% if RCOND of inv(R) is above R_mat_tol,
                             %% the learning algorithm switches to SG
                             %% (active if learning==1)
 
-ini_cds = 0;   %% set == 1 if initial conditions for calculating REE are 
+ini_cds = 0;   %% set == 1 if initial conditions for calculating REE are
                %% already defined
-                               
+
 bigT_vec = [1 2 3 4];   %% vector containing the expectations horizons (active if exp_gen == 1 )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -128,21 +121,21 @@ bigT_vec = [1 2 3 4];   %% vector containing the expectations horizons (active i
 %% Store/impulse resp. and forecast options
 
 if imp_resp == 1
-    
+
     store = 0;
-    
+
 else
-    
+
     Regressors_ini = 0;  R_mat_ini = 0; state_ini = 0;
     OMEGA_c_ini = 0;OMEGA_0_ini = 0;
 
 end
 
 if exp_gen == 0
-    
+
 Exp_Y_var = 0;
 Exp_R_1Q = 0;Exp_R_3Q = 0; Exp_w_1Q = 0;
-Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0; 
+Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0;
 Exp_rk_1Q = 0; Exp_rk_2Q = 0;Exp_rk_3Q = 0;Exp_rk_4Q = 0;
 
 end
@@ -178,16 +171,16 @@ cap = 7;
 gamma_x = 13;
 
 
-  
+
 
 ct2 = ct1;
 
 if g_gain < 0 | g_gain > 0.025
-     
+
      ct2 = 1;
-     
+
 end
- 
+
 if ct2 == 0
 
 
@@ -213,14 +206,14 @@ n_shocks = size(C,2); % n of shocks in the model
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if gen_shocks == 1
-    
+
 RND_Mat = randn(n_shocks,s_length);
 
 save SHOKS_mat RND_Mat
 
 else
-    
-%% comment if use this file as a function    
+
+%% comment if use this file as a function
  % load SHOKS_mat;
 
  if full == 1
@@ -228,7 +221,7 @@ else
 %% If use this file as function
 RND_Mat = epsZ;
 
- else 
+ else
 
 RND_Mat = epsZ_imp1;
 
@@ -239,11 +232,11 @@ end  %% ends gen_shock loop
 %% Checking dimensions of the shock matrix
 
 if size(RND_Mat,1) ~= n_shocks
-    
+
     disp('wrong number of simulated shocks!')
-    
+
     return
-    
+
 end
 
 
@@ -270,29 +263,29 @@ Shocks = diag(S_mat)*RND_Mat;
  %% Checking consistency with the model
 
 if n_x > n_var
-    
+
     disp('too many regressors!')
-    
+
     return
-    
+
 end
 
 if n_eq > n_var
-    
+
     disp('too many equations to estimate!')
-    
+
     return
-    
+
 end
 
  %% Regression coefficients
 
 for ctn = 1:n_eq
-    
+
     Regressors{ctn} = zeros(n_x+n_c,1);
-    
+
     Regressors1{ctn} = zeros(n_x+n_c,1);
-    
+
     Regressors_RE{ctn} = zeros(n_x+n_c,1);
 end
 
@@ -321,7 +314,7 @@ X_vec = zeros(n_c+n_x,1);
  %% Solve the model under RE
 
 if ini_cds == 0
-    
+
 
 ini_cond_0 = zeros(n_var,1);
 
@@ -337,7 +330,7 @@ end
 
 
  [OMEGA_0_RE OMEGA_c_RE ct1] = REDS_SOLDS_Model_Sept_2009(x);
- 
+
 
 %% Solve with T-map iteration (double check solution!, other variables)
 
@@ -356,13 +349,13 @@ ini_cond = OMEGA_c_RE;
 
 %% Check E-Stability
 
-% [eig_vec_0,eig_vec_c] = Estab_fun(A,C,invA0,OMEGA_0_RE,OMEGA_c_RE,T_c_RE,T_Ls_RE,k_y,disc); 
+% [eig_vec_0,eig_vec_c] = Estab_fun(A,C,invA0,OMEGA_0_RE,OMEGA_c_RE,T_c_RE,T_Ls_RE,k_y,disc);
 
 
 % %% updates initial condition for next time...NOT USED IN THIS VERSION
-% 
+%
 % ini_cond = OMEGA_c_RE;
-% 
+%
 % ini_cond_0 = OMEGA_0_RE;
 
 if full == 1
@@ -383,21 +376,21 @@ OMEGA_0 = OMEGA_0_RE;
 
 
 if learning == 1
-    
-   
-        
+
+
+
 ini_VCV = VCV_Model(T_L_RE,T_s_RE,var_model);
 
 
 
 
  %% here we pick the regressors. In this case, capital:
- 
+
 R_mat(n_c+1:end,n_c+1:end) = ini_VCV(cap,cap);
 
- 
 
-   
+
+
 end %% ends learning vcv loop
 
 
@@ -413,48 +406,48 @@ end %% end full==1 loop
 %% Set initial beliefs
 
 for ctn = 1:n_eq
-    
-    
+
+
     Regressors_RE{ctn}(n_c+1:end) = Regressors{ctn}(n_c+1:end);
-    
+
     if full == 1
-    
-      Regressors{ctn}(n_c+1:end) = OMEGA_c(ctn,cap);  
-      
+
+      Regressors{ctn}(n_c+1:end) = OMEGA_c(ctn,cap);
+
     else
-    
+
       Regressors{ctn}(n_c+1:end) = ini1{ctn}(n_c+1:end);
-        
+
     end
-    
+
 end
-    
+
 if full == 0 %% THIS IS USED WHEN PLOTTING IMPULSE RESPONSES
 
 
-%% 1. Initial intercept is not zero! 
-    
+%% 1. Initial intercept is not zero!
+
 if n_c == 1
-    
+
     for ctn = 1:n_eq
-    
+
     Regressors{ctn}(1) = ini1{ctn}(1);
- 
+
     end
- 
-end 
+
+end
 
 
 %% 2. initial second-moments matrix
- 
- 
+
+
  R_mat = ini2;
- 
+
 
 %% 3. initial OMEGA_0 and OMEGA_c
 
  if n_c == 1
-    
+
 
 
 OMEGA_0 = ini5;
@@ -464,14 +457,14 @@ OMEGA_0 = ini5;
 
 if n_x > 0
 
-  
- 
+
+
  OMEGA_c(:,cap) = ini4;
 
 end
 
 end %% closes the full==0 loop
- 
+
 
 
 %% Store the evolution of estimation coefficients
@@ -480,8 +473,8 @@ end %% closes the full==0 loop
 if store == 1
 
       for ctn = 1:n_eq
-        
-COEFF_STORE{ctn} = zeros(n_c+n_x,size(RND_Mat,2));   
+
+COEFF_STORE{ctn} = zeros(n_c+n_x,size(RND_Mat,2));
 
       end
 
@@ -492,9 +485,9 @@ end
 %% constant in X_vec
 
 if n_c == 1
-    
-    X_vec(1) = 1;  
-    
+
+    X_vec(1) = 1;
+
 end
 
 
@@ -507,16 +500,16 @@ Y_var = zeros(n_var,size(RND_Mat,2));
 %% ...unless full == 0...
 
 if full == 0
-    
+
  Y_var(:,1) = ini3;
- 
+
 end
 
 
 
 if exp_gen == 1
 
-    
+
 %% AGENTS' FORECATS: select the number of variable (< than n_var) to forecast
 
 Exp_R_1Q = zeros(1,size(RND_Mat,2));
@@ -542,7 +535,7 @@ Exp_rk_4Q = zeros(1,size(RND_Mat,2));
 
 
 for ctn_e = 1:length(bigT_vec)
-    
+
 Exp_Y_var{ctn_e} = zeros(n_var,size(RND_Mat,2));
 
 
@@ -554,14 +547,14 @@ end %% close exp_gen loop
 
 
 % %% Projection facility: NOT USED FOR NOW...
-% 
+%
 % %% Get std from estimation without feedback
-% 
+%
 % if p_f == 1
-% 
+%
 %     [f_vec,x_vec,f_n_vec,mu_vec,sigma_vec] = Model_Simul_no_feedback(g_gain);
-% 
-% end 
+%
+% end
 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%
@@ -571,8 +564,8 @@ end %% close exp_gen loop
 
 
 for ctn_s = 2:size(RND_Mat,2)
-    
-    
+
+
 %% uncomment if want to display the counter
 % ctn_s
 
@@ -583,12 +576,12 @@ if learning == 1
 
     [T_0 T_L T_s T_c T_Ls] = ALM_fun(A,C,invA0,OMEGA_0,OMEGA_c,k_y,disc);
 
-    
+
     Y_var(:,ctn_s) = T_0+T_L*Y_var(:,ctn_s-1)+T_s*Shocks(:,ctn_s-1);
 
-    
+
 else
-    
+
     Y_var(:,ctn_s) = T_0_RE+T_L_RE*Y_var(:,ctn_s-1)+T_s_RE*Shocks(:,ctn_s-1);
 
 end
@@ -598,67 +591,67 @@ end
 %% checking for exploding series
 
 if max(Y_var(:,ctn_s)) > 1000
- 
+
    disp('exploding series')
 
    ct2 = 1; Y_var = 0; Exp_Y_var = 0;
             Regressors_ini = 0; R_mat_ini = 0;
             state_ini = 0; OMEGA_c_ini = 0; OMEGA_0_ini = 0;
            Exp_R_1Q = 0;Exp_R_3Q = 0; Exp_w_1Q = 0;
-Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0; 
+Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0;
 Exp_rk_1Q = 0; Exp_rk_2Q = 0;Exp_rk_3Q = 0;Exp_rk_4Q = 0;
 
 return
-   
+
 end
-  
+
 
 %% Generates expectations
 
-if exp_gen == 1 
+if exp_gen == 1
 
-%% Computes forecasts of basic model variables  
-  
+%% Computes forecasts of basic model variables
+
 for ctn_e = 1:length(bigT_vec)
-    
+
   forcast_vec = ...
       inv(eye(n_var)-OMEGA_c)*(eye(n_var)-OMEGA_c^(bigT_vec(ctn_e)))*OMEGA_0+...
       OMEGA_c^(bigT_vec(ctn_e))*Y_var(:,ctn_s);
-  
-  %% This stores the forecast we are interested in. You have to remember 
-  %% the location of the variable you are interested in forecasting (as in 
+
+  %% This stores the forecast we are interested in. You have to remember
+  %% the location of the variable you are interested in forecasting (as in
   %% the Model file.
-  
+
   %% Timing: the first column of the expectations vector describes
   %% expectations taken in period 2 (ctn_s=2) on varibables in periods 3
   %% and beyond.
-  
-  Exp_Y_var{ctn_e}(:,ctn_s) = forcast_vec;
-  
-  %% Reminder (double check!): the order is: rk,w,y.
-      
-end 
 
- 
+  Exp_Y_var{ctn_e}(:,ctn_s) = forcast_vec;
+
+  %% Reminder (double check!): the order is: rk,w,y.
+
+end
+
+
 %% HERE WE SIMULATE AGENT'FORECASTS, TO BE COMPARED WITH DATA
 
- 
+
  %% 1. Forecast of return on the riskless asset
 
   Exp_R_1Q(ctn_s) = Exp_Y_var{1}(bond,ctn_s);
-  
-  
+
+
   Exp_R_3Q(ctn_s) = Exp_Y_var{3}(bond,ctn_s);
 
-  
+
  %% 2. wage forecast
 
- 
-Exp_w_1Q(ctn_s) = Exp_Y_var{1}(wage,ctn_s); 
+
+Exp_w_1Q(ctn_s) = Exp_Y_var{1}(wage,ctn_s);
 
 Exp_w_2Q(ctn_s) = Exp_Y_var{2}(wage,ctn_s);
 
-Exp_w_3Q(ctn_s) = Exp_Y_var{3}(wage,ctn_s); 
+Exp_w_3Q(ctn_s) = Exp_Y_var{3}(wage,ctn_s);
 
 Exp_w_4Q(ctn_s) = Exp_Y_var{4}(wage,ctn_s);
 
@@ -676,7 +669,7 @@ Exp_rk_4Q(ctn_s) = Exp_Y_var{4}(rk,ctn_s);
 
 
 
-end %% closes exp_gen loop 
+end %% closes exp_gen loop
 
 
 
@@ -686,19 +679,19 @@ end %% closes exp_gen loop
 
 if learning == 1
 
-    
-    
-    
+
+
+
 %% update gain
 
 if cg == 0
-    
+
     g = (ctn_s+ini_gain)^(-1);
-    
+
 else
-    
+
     g = g_gain;
-    
+
 end %% closes the if_cg loop
 
 
@@ -710,12 +703,12 @@ end %% closes the if_cg loop
 
 
 
-Y_vec = Y_var(1:n_eq,ctn_s); 
+Y_vec = Y_var(1:n_eq,ctn_s);
 
 
 
 if length(Y_vec) ~= n_eq
-    
+
     disp('n. of regression eqs. different from n. of dependent. vars.')
 break
 
@@ -726,36 +719,36 @@ end
 %% update precision matrix
 
 if sga == 0
-   
+
  R_mat1 = R_mat+g*(X_vec*X_vec'-R_mat);
-  
- 
+
+
  %% checking the invertibility if the R matrix
- 
+
   if rcond(R_mat1) < R_mat_tol
- 
+
    disp('warning: singular R matrix')
 
    ct2 = 1; Y_var = 0; Exp_Y_var = 0;
             Regressors_ini = 0; R_mat_ini = 0;
             state_ini = 0; OMEGA_c_ini = 0; OMEGA_0_ini = 0;
            Exp_R_1Q = 0;Exp_R_3Q = 0; Exp_w_1Q = 0;
-Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0; 
+Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0;
 Exp_rk_1Q = 0; Exp_rk_2Q = 0;Exp_rk_3Q = 0;Exp_rk_4Q = 0;
 
             return
-   
+
   end
-  
-     
+
+
   R_inv = inv(R_mat1);
- 
- 
-  
+
+
+
 else   %% sga loop
-    
-    
- 
+
+
+
 %% Standard SG
 
 % NOT AVAILABLE
@@ -774,7 +767,7 @@ end %% closes the if_sga loop
 %% update estimates
 
 for ctn = 1:n_eq
-    
+
 
  Regressors1{ctn} = Regressors{ctn}+g*R_inv*X_vec*(Y_vec(ctn)-Regressors{ctn}'*X_vec);
 
@@ -783,29 +776,29 @@ for ctn = 1:n_eq
 end
 
 
- 
+
 
 
 %% Projection facility: NOT ACTIVE NOW...
 
 % if p_f == 1
-%     
-%     
+%
+%
 %    for ctn = 1:n_eq
-%        
+%
 %        for ctn1 = 1:n_x+n_c
-%            
+%
 %        if abs(Regressors1{ctn}(ctn1)-Regressors_RE{ctn}(ctn1))...
 %                >= pf_k*sigma_vec{ctn}(ctn1)
-%            
+%
 %                 Regressors1{ctn}(ctn1) = Regressors{ctn}(ctn1);
 %            end
-%        
-%        end 
-% 
+%
+%        end
+%
 %    end
-%    
-%     
+%
+%
 % end %% ends projection facility loop
 
 
@@ -813,9 +806,9 @@ end
 %% Projection facility on CAPITAL coefficient
 
 if abs(Regressors1{cap}(1+n_c)) > 0.99
-    
+
     Regressors1{cap}(1+n_c) = Regressors{cap}(1+n_c);
-    
+
 end
 
 
@@ -823,13 +816,13 @@ end
 %% Keeping track of the estimates
 
 if store == 1
-    
+
     for ctn = 1:n_eq
-        
-COEFF_STORE{ctn}(:,ctn_s-1) = Regressors1{ctn};   
+
+COEFF_STORE{ctn}(:,ctn_s-1) = Regressors1{ctn};
 
     end
-    
+
 end %% close store loop
 
 
@@ -837,18 +830,18 @@ end %% close store loop
 %% Update regressors vectors
 
 for ctn = 1:n_eq
-       
-    
+
+
 Regressors{ctn} = Regressors1{ctn};
- 
-    
+
+
 end
 
 
 %%... and the precision matrix
 
 if sga == 0
-    
+
 R_mat = R_mat1;
 
 end
@@ -858,12 +851,12 @@ end
 
 if feedback == 1
 
-    
-    
+
+
  if n_c == 1
-    
+
 for ctn = 1:n_eq
-    
+
 OMEGA_0(ctn) = Regressors{ctn}(1);
 
 end
@@ -873,13 +866,13 @@ end
 
 if n_x > 0
 
-for ctn = 1:n_eq    
-  
+for ctn = 1:n_eq
+
 
   OMEGA_c(ctn,cap) = Regressors{ctn}(n_c+1:end);
 
 
- 
+
 end
 
 end
@@ -891,8 +884,8 @@ end
 
 forecst_RE = T_0_fre+T_L_fre*Y_var(:,ctn_s);
 
-%forecstw(ctn_s) = forecst_RE(wage);    
-%Exp_w_1Q(ctn_s) = forecst_RE(wage);   
+%forecstw(ctn_s) = forecst_RE(wage);
+%Exp_w_1Q(ctn_s) = forecst_RE(wage);
 
 end %% closes the if_feedback command
 
@@ -905,12 +898,12 @@ end %% closes the if_learning command
 if imp_resp == 1
 
 if ctn_s == sim_L+1  %% here: -- (end of 'training' sample)
- 
-    
-    if full == 1
-    
 
-    
+
+    if full == 1
+
+
+
 %% 1. Regressors_ini
 
 Regressors_ini = Regressors;
@@ -927,8 +920,8 @@ state_ini = Y_var(:,ctn_s);
 
 OMEGA_c_ini  = OMEGA_c(:,cap);
 
-OMEGA_0_ini = OMEGA_0; 
- 
+OMEGA_0_ini = OMEGA_0;
+
 end %% ends full == 1 loop
 
 
@@ -936,43 +929,43 @@ end %% ends ctn_s == sim_L+1 loop
 
 
   if full == 0
-    
-        
+
+
  Regressors_ini = 0;  R_mat_ini = 0; state_ini = 0;
  OMEGA_c_ini = 0; OMEGA_0_ini = 0;
-  
+
   end
- 
-end %% ends imp_resp loop 
+
+end %% ends imp_resp loop
 
  end %% end of simulation
- 
- 
 
 
- 
+
+
+
 %%  SAVE estimation coefficients
- 
+
  if store == 1
-     
+
      save COEFF_STORE_mat COEFF_STORE
-     
+
  end
 
- 
- 
-        
+
+
+
 end  %% end the ct2 loop
 
  if ct2 == 1
-            
-            Y_var = 0; 
+
+            Y_var = 0;
             Regressors_ini = 0; R_mat_ini = 0;
             state_ini = 0; OMEGA_c_ini = 0; OMEGA_0_ini = 0;
          Exp_R_1Q = 0;Exp_R_3Q = 0; Exp_w_1Q = 0;
-Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0; 
+Exp_w_2Q = 0;Exp_w_3Q = 0;Exp_w_4Q = 0;
 Exp_rk_1Q = 0; Exp_rk_2Q = 0;Exp_rk_3Q = 0;Exp_rk_4Q = 0;
-   
-            
+
+
         end
- 
+
--- Eusepi_Preston_2011_Replication_Pkg/Model/Impulse-responses/Main_imp_resp_Sept_2009.m
+++ Eusepi_Preston_2011_Replication_Pkg/Model/Impulse-responses/Main_imp_resp_Sept_2009.m
@@ -12,13 +12,9 @@ addpath(fullfile(pkg_dir, 'Common'), '-end');
 
 %% The file generates 1000 impulse responses, depending on the initial
 %% state of the economy
-
-global epsZ 
-
-
 T_imp = 64; %% T_imp-1 is the number of periods in the IR
 
-sim_L = 2000; %% this denotes the total number of observations that have 
+sim_L = 2000; %% this denotes the total number of observations that have
               %% to be discarded before generating the IR
 
 T_tot = sim_L+T_imp; %% total length of simulation
@@ -48,7 +44,7 @@ store_c  = imp_resp_store; %% set == 1 to store matrix of impulse responses
 
 
 opt_x = exp(-0.034);
- 
+
 opt_x1 = opt_x;
 
   x = [1;0;1;1;0.0001;0.002]; %% parameters full model
@@ -57,8 +53,8 @@ opt_x1 = opt_x;
 % %  param(3) = 1.2; %% sigma
 % %  param(4) = 0; %% simple RBC (Takes 1 or zero)
 % %  param(5) = 0; %% constant gain
- 
- S_mat = [opt_x];  
+
+ S_mat = [opt_x];
 
 
 fb = 1;
@@ -80,11 +76,11 @@ imp_resp = 1;
 
 %% Define the IR matrices
 
-n_impulse_resp = 14; %% number of impulse responses that you want to plot. 
+n_impulse_resp = 14; %% number of impulse responses that you want to plot.
                      %% The order is defines in the Observed_variables file
 
 for j = 1:n_impulse_resp
-    
+
 imp_resp_vec{j} = zeros(n_draws,T_imp-1);  %%remember, the vector is one unit shorter...
 
 median_imp_resp_vec{j} = zeros(1,T_imp-1);
@@ -104,8 +100,8 @@ disp('start computing impulse responses')
 
 
 for ctn = 1:n_draws
-    
-epsZ_full = randn(1,T_tot); %% must correspond to the number of shocks in 
+
+epsZ_full = randn(1,T_tot); %% must correspond to the number of shocks in
                               %% the model
 
 
@@ -125,7 +121,7 @@ ini1 = 0; ini2 = 0; ini3 = 0; ini4 = 0; ini5 = 0;
 
 
  %% Model parameters that we need
- % vars location (needed below) 
+ % vars location (needed below)
  % jump
 
 % jump
@@ -151,61 +147,61 @@ gamma_x = 13;
 
 
  %% Simulate stationary model
- 
+
 % [Y_var,Exp_R_1Q,Exp_R_3Q,Exp_gy_1Q,Exp_gy_4Q,Exp_gn_1Q,Exp_gn_4Q,...
 %               Regressors_ini,R_mat_ini,state_ini,OMEGA_c_ini,OMEGA_0_ini,ct2] = ...
-%          Model_Simul_Sept_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L);
-% 
+%          Model_Simul_Sept_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L,epsZ);
+%
 
 [Y_var,Exp_R_1Q,Exp_R_3Q,Exp_w_1Q,Exp_w_2Q,Exp_w_3Q,...
          Exp_w_4Q,Exp_rk_1Q,Exp_rk_2Q,Exp_rk_3Q,Exp_rk_4Q,...
               Regressors_ini,R_mat_ini,state_ini,OMEGA_c_ini,OMEGA_0_ini,ct2] = ...
-         Model_Simul_Oct_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L);
+         Model_Simul_Oct_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L,epsZ);
 
 
- %% Compute deviation from BGP        
-         
+ %% Compute deviation from BGP
+
  y_growth_vec = [Y_var(wage,sim_L+2:end);Y_var(cons,sim_L+2:end);Y_var(invst,sim_L+2:end);Y_var(output,sim_L+2:end)]-...
                      [Y_var(wage,sim_L+1:end-1);Y_var(cons,sim_L+1:end-1);Y_var(invst,sim_L+1:end-1);Y_var(output,sim_L+1:end-1)]+...
                       ones(4,1)*Y_var(gamma_x,sim_L+2:end);
-                  
-                 
-                  
+
+
+
   %% NOTE: the imp-resp shock impacts at sim_L+2. (That is, the initial
   %% state saved corresponds to observation sim_L_1.) Accordingly, the
   %% impulse response has a lenght of T_imp-1, where the first observation
   %% is the impact response of the shock. Thus, the growth rate is defined
   %% as impact response (sim_L+2) less initial state (sim_L+1).
-                      
-                      
+
+
 y_detr_vec = zeros(size(y_growth_vec,1),size(y_growth_vec,2));
 
 
-y_detr_vec(:,1) = y_growth_vec(:,1);  
- 
+y_detr_vec(:,1) = y_growth_vec(:,1);
+
  %% NOTE: the initial observation corresponds to the initial growth rate.
 
- 
+
  for j = 2:size(y_growth_vec,2)
- 
+
  y_detr_vec(:,j) = y_growth_vec(:,j)+y_detr_vec(:,j-1);
-     
+
  end
 
- 
+
 
  %% Compute impresp1
- 
+
  % LIST of VARS: {'w','c','i','y','b','h','rk','we','Exprk1','Expw1','Exprk20','Exprk40','Expwe20','Expwe40'}
- 
+
  impresp1 = [y_detr_vec;Y_var(bond,sim_L+2:end);Y_var(hours,sim_L+2:end);...
              Y_var(rk,sim_L+2:end);Y_var(wage,sim_L+2:end);...
              Exp_rk_1Q(sim_L+2:end);Exp_w_1Q(sim_L+2:end);...
              Exp_rk_3Q(sim_L+2:end);Exp_rk_4Q(sim_L+2:end);...
              Exp_w_3Q(sim_L+2:end);Exp_w_4Q(sim_L+2:end);];
- 
- 
- 
+
+
+
 %% Add 1 std shock...
 
 full = 0;
@@ -226,45 +222,45 @@ mat_imp = epsZ_full(T_tot-T_imp+1:end)+[1/opt_x1,zeros(1,T_imp-1)];
 
 
  epsZ_imp1 = mat_imp;
- 
- 
+
+
  %% Simulate stationary model
- 
+
 % [Y_var,Exp_R_1Q,Exp_R_3Q,Exp_gy_1Q,Exp_gy_4Q,Exp_gn_1Q,Exp_gn_4Q,...
 %               Regressors_ini,R_mat_ini,state_ini,OMEGA_c_ini,OMEGA_0_ini,ct2] = ...
-%          Model_Simul_Sept_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L);
+%          Model_Simul_Sept_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L,epsZ);
 
- 
+
 [Y_var,Exp_R_1Q,Exp_R_3Q,Exp_w_1Q,Exp_w_2Q,Exp_w_3Q,...
          Exp_w_4Q,Exp_rk_1Q,Exp_rk_2Q,Exp_rk_3Q,Exp_rk_4Q,...
               Regressors_ini,R_mat_ini,state_ini,OMEGA_c_ini,OMEGA_0_ini,ct2] = ...
-         Model_Simul_Oct_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L);     
-     
-     
- %% Compute deviation from BGP        
-         
+         Model_Simul_Oct_2009(x,S_mat,fb,lern,exp_gen,imp_resp,full,epsZ_imp1,ini1,ini2,ini3,ini4,ini5,sim_L,epsZ);
+
+
+ %% Compute deviation from BGP
+
  y_growth_vec = [Y_var(wage,2:end);Y_var(cons,2:end);Y_var(invst,2:end);Y_var(output,2:end)]-...
                      [Y_var(wage,1:end-1);Y_var(cons,1:end-1);Y_var(invst,1:end-1);Y_var(output,1:end-1)]+...
-                      ones(4,1)*Y_var(gamma_x,2:end); 
-                  
-                 
+                      ones(4,1)*Y_var(gamma_x,2:end);
+
+
                 %% NOTE: the imp-resp shock impacts at sim_L+2
-                      
-                      
+
+
 y_detr_vec = zeros(size(y_growth_vec,1),size(y_growth_vec,2));
 
 
-y_detr_vec(:,1) = y_growth_vec(:,1);  
- 
+y_detr_vec(:,1) = y_growth_vec(:,1);
 
- 
+
+
  for j = 2:size(y_growth_vec,2)
- 
+
  y_detr_vec(:,j) = y_growth_vec(:,j)+y_detr_vec(:,j-1);
-     
+
  end
 
- 
+
   % LIST of VARS: {'w','c','i','y','b','h','rk','we','Exprk1','Expw1','Exprk20','Exprk40','Expwe20','Expwe40'}
 
  impresp2 = [y_detr_vec;Y_var(bond,2:end);Y_var(hours,2:end);...
@@ -272,14 +268,14 @@ y_detr_vec(:,1) = y_growth_vec(:,1);
              Exp_rk_1Q(2:end);Exp_w_1Q(2:end);...
              Exp_rk_3Q(2:end);Exp_rk_4Q(2:end);...
              Exp_w_3Q(2:end);Exp_w_4Q(2:end);];
- 
- 
-         
- 
+
+
+
+
 
 
 for j = 1:n_impulse_resp
-    
+
 imp_resp_vec{j}(ctn,:) = impresp2(j,:)-impresp1(j,:);
 
 end
@@ -295,9 +291,9 @@ end %% end draws loop
 %% Median impulse responses
 
 for j1 =1:n_impulse_resp
-    
+
     for j2 = 1:T_imp-1
-    
+
 median_imp_resp_vec{j1}(j2) = median(imp_resp_vec{j1}(:,j2));
 
 end
@@ -318,13 +314,13 @@ low_band{j1}(j2) = vec_sort(band_down);  %%$work in progress (define percentile?
 
 up_band{j1}(j2) =  vec_sort(band_up);
 
-  end 
+  end
 
 end
 
 toc
 
- 
+
 
 if store_c == 1
 
@@ -341,4 +337,4 @@ if store_c == 1
     imp_resp_output.(imp_resp_output_var) = imp_resp_vec;
 
     save(fullfile(main_dir, imp_resp_output_file), '-struct', 'imp_resp_output');
-end    
+end
